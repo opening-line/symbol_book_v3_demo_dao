b6db9d71cf22f4f0aba4866d5d2e7efbf5ac96d1
inatatsu <tatsuhiro.9699@gmail.com>
2024-10-29 15:42:56 +0900

    :sparkles: client setting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,39 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { KeyPair, SymbolFacade } from 'symbol-sdk/symbol'
+import { Network } from 'symbol-sdk/nem'
+import { PrivateKey } from 'symbol-sdk'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const onClick = () => {
+    const privateKey = import.meta.env.VITE_PRIVATE_KEY
+    const account = new KeyPair(new PrivateKey(privateKey))
+    const message = new Uint8Array([0x00, ...new TextEncoder().encode('test transaction')])
+
+    const facade = new SymbolFacade(Network.TESTNET.toString())
+
+    const tx = facade.transactionFactory.create({
+      type: "transfer_transaction_v1",
+      signerPublicKey: account.publicKey,
+      deadline: facade.now().addHours(2).timestamp,
+      recipientAddress: facade.network.publicKeyToAddress(account.publicKey),
+      mosaics: [],
+      message,
+      fee: 1000000n
+    })
+    const signature = facade.signTransaction(account, tx)
+    const jsonPayload = facade.transactionFactory.static.attachSignature(tx, signature);
+
+    console.log({jsonPayload})
+
+    fetch(
+      new URL('/transactions', 'https://sym-test-03.opening-line.jp:3001'),
+      { method: 'PUT', headers: { 'Content-Type': 'application/json' }, body: jsonPayload }
+    ).then((res) => res.json());
+
+  }
 
   return (
     <>
@@ -16,18 +45,12 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>DEMO APP</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={onClick}>
+          click
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
