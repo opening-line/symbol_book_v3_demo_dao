5448b8851f8f7a242d830b51f791b8857204a247
inatatsu <tatsuhiro.9699@gmail.com>
2024-11-11 17:58:01 +0900

    :sparkles: SSSで署名

--- client/src/pages/DAO/Create.tsx
+++ client/src/pages/DAO/Create.tsx
@@ -1,32 +1,35 @@
-import { PrivateKey, utils } from "symbol-sdk"
+import { utils } from "symbol-sdk"
 import { models, Network, SymbolFacade } from "symbol-sdk/symbol"
+
+import { setTransactionByPayload, requestSignCosignatureTransaction, getActivePublicKey } from "sss-module"
 
 const NODE_URL = 'https://sym-test-03.opening-line.jp:3001'
 
 export const CreateDAOPage: React.FC = () => {
   const sign = async () => {
 
-  const payload = await fetch("http://localhost:3000/admin/new", {
+    const ownerPublicKey = getActivePublicKey()
+
+  const {payload} = await fetch("http://localhost:3000/admin/new", {
     method: 'POST',
     body: JSON.stringify({
       daoName: "test dao dao",
-      ownerPublicKey: "7F544D00FD823B60EDE4CB604AA2366FA2AD20CF9A9C25BB41A1B15B99838397"
+      ownerPublicKey
     }),
   })
     .then(response => response.json())
 
-  console.log({payload})
-  return
-
-    const pk = '39324C83D5427071E2D56513558A56D50EF874A92CCD53F5AE7976AE1473BBA6'
     const facade = new SymbolFacade(Network.TESTNET)
-    const acc = facade.createAccount(new PrivateKey(pk))
 
     const tx = models.AggregateCompleteTransactionV2.deserialize(utils.hexToUint8(payload))
-
-    const cosign = facade.cosignTransaction(acc.keyPair, tx)
-
-    tx.cosignatures.push(cosign)
+    setTransactionByPayload(payload)
+      
+    const cosignedTx = await requestSignCosignatureTransaction()
+  
+    const cosignature = new models.Cosignature()
+    cosignature.signature.bytes = utils.hexToUint8(cosignedTx.signature)
+    cosignature.signerPublicKey.bytes = utils.hexToUint8(cosignedTx.signerPublicKey)
+    tx.cosignatures.push(cosignature)
 
     const jsonPayload2 = `{"payload":"${utils.uint8ToHex(tx.serialize())}"}`
 
