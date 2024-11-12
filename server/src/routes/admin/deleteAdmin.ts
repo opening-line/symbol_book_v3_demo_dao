478dca6ff4abae8af4a4ac9d66280d143808b2df
inatatsu <tatsuhiro.9699@gmail.com>
2024-11-12 16:12:29 +0900

    :sparkles: adminの追加削除のロジックのアグボン化

--- server/src/routes/admin/deleteAdmin.ts
+++ server/src/routes/admin/deleteAdmin.ts
@@ -1,21 +1,19 @@
 import type { Context } from "hono";
-import { PrivateKey, PublicKey, utils } from "symbol-sdk";
-import { Address, descriptors, KeyPair, models, SymbolFacade } from "symbol-sdk/symbol";
+import { PrivateKey, PublicKey } from "symbol-sdk";
+import { Address, descriptors, models, SymbolFacade } from "symbol-sdk/symbol";
 import { Config } from "../../utils/config";
 import { createDummy } from "../../functions/createDummy";
 import { env } from "hono/adapter";
 import { deleteMultisig } from "../../functions/deleteMultisig";
+import { createHashLock } from "../../functions/createHashLock";
 
 export const deleteAdmin = async (c: Context) => {
-  const { daoId, addresses } = await c.req.json() as { daoId: string, ownerPublicKey: string, addresses: string[] }
-
-  // const daoId = 'BCEECAE597FAD80A7BB81F5BDBBD39C4D2869F6128405C04F7EC23C7227B27D6'
-  // const addresses = ['TDSCLQDYNIAQQGU6MRS7THIUOO526RHFNVCRFUI']
+const { daoId, addresses } = await c.req.json() as { daoId: string, ownerPublicKey: string, addresses: string[] }
 
   const ENV = env<{ PRIVATE_KEY: string }>(c)
   const facade = new SymbolFacade(Config.NETWORK)
   const masterAccount = facade.createAccount(new PrivateKey(ENV.PRIVATE_KEY))
-  // Delete admin to DAO
+  // Add admin to DAO
 
   const daoAccount = facade.createPublicAccount(new PublicKey(daoId))
 
@@ -32,54 +30,70 @@ export const deleteAdmin = async (c: Context) => {
     dummyTransaction
   ]
 
-    // TODO: 署名
+  
+  // TODO: アグリゲート
   const txHash = SymbolFacade.hashEmbeddedTransactions(innerTransactions)
-  const aggregateDes = new descriptors.AggregateCompleteTransactionV2Descriptor(
+  const aggregateDes = new descriptors.AggregateBondedTransactionV2Descriptor(
     txHash,
     innerTransactions,
   )
-  const tx = models.AggregateCompleteTransactionV2.deserialize(
+  const tx = models.AggregateBondedTransactionV2.deserialize(
     facade
-      .createTransactionFromTypedDescriptor(
-        aggregateDes,
-        masterAccount.publicKey,
-        Config.FEE_MULTIPLIER,
-        Config.DEADLINE_SECONDS,
-      )
-      .serialize(),
+    .createTransactionFromTypedDescriptor(
+      aggregateDes,
+      masterAccount.publicKey,
+      Config.FEE_MULTIPLIER,
+      Config.DEADLINE_SECONDS,
+    )
+    .serialize(),
   )
 
   const signatureMaster = masterAccount.signTransaction(tx)
 
-  facade.transactionFactory.static.attachSignature(tx, signatureMaster)
+  const jsonPayload = facade.transactionFactory.static.attachSignature(tx, signatureMaster)
 
-  const newbie = new KeyPair(new PrivateKey('12F0717F0B48D9A4EE355FEAF9C0FD9F56136F9AF15857666C746A0CA4CC21EF')) // TDSCL...
-  const cosign = facade.cosignTransaction(newbie, tx)
+  const hashAgg = facade.hashTransaction(tx)
+  
+  // TODO: HashLock
+  const hashLock = createHashLock(hashAgg)
+  const hashLockTransaction = facade.createTransactionFromTypedDescriptor(hashLock, masterAccount.publicKey, Config.FEE_MULTIPLIER, Config.DEADLINE_SECONDS)
+  
+  
+  const signatureMasterHashLock = masterAccount.signTransaction(hashLockTransaction)
+  
+  const jsonPayloadHashLock = facade.transactionFactory.static.attachSignature(hashLockTransaction, signatureMasterHashLock)
+  
+  const hashHL = facade.hashTransaction(hashLockTransaction)
 
-  tx.cosignatures.push(cosign)
-
-  const owner = new KeyPair(new PrivateKey('39324C83D5427071E2D56513558A56D50EF874A92CCD53F5AE7976AE1473BBA6')) // TD61E...
-  const cosign2 = facade.cosignTransaction(owner, tx)
-
-  tx.cosignatures.push(cosign2)
-
-  const jsonPayload2 = `{"payload":"${utils.uint8ToHex(tx.serialize())}"}`;
-
-  const hash = facade.hashTransaction(tx)
 
   const sendRes = await fetch(
       new URL('/transactions', Config.NODE_URL),
-      { method: 'PUT', headers: { 'Content-Type': 'application/json' }, body: jsonPayload2 }
+      { method: 'PUT', headers: { 'Content-Type': 'application/json' }, body: jsonPayloadHashLock }
   )
       .then((res) => res.json());
   console.log(sendRes);
 
   await new Promise((resolve) => setTimeout(resolve, 1000));
 
-  const statusRes = await fetch(new URL("/transactionStatus/" + hash, Config.NODE_URL))
+  for (let i = 0; i < 100; i++) {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    const hashLockStatus = await fetch(
+        new URL("/transactionStatus/" + hashHL, Config.NODE_URL)
+    )
+        .then((res) => res.json());
+        console.log(hashLockStatus);
+    if (hashLockStatus.group === "confirmed") {
+      break;
+    }
+  }
+
+    const sendAggRes = await fetch(
+      new URL('/transactions/partial', Config.NODE_URL),
+      { method: 'PUT', headers: { 'Content-Type': 'application/json' }, body: jsonPayload }
+  )
       .then((res) => res.json());
-  console.log(statusRes);
+  console.log(sendAggRes);
   
 
-  return c.json({ message: "Hello addAdmin" });
+  return c.json({ message: "Hello deleteAdmin" });
 }
