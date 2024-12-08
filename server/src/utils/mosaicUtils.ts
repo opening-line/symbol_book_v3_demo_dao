9d073c3df693cdf1628ffcda63a0f804004409bf
ry0-cha-ng <ry0changmw@gmail.com>
2024-12-08 18:06:27 +0900

    :sparkles: server/src/utils に関数コメントを追加

--- server/src/utils/mosaicUtils.ts
+++ server/src/utils/mosaicUtils.ts
@@ -1,3 +1,9 @@
+/**
+ * 可分性を考慮したモザイク量を取得
+ * @param amount 量
+ * @param divisibility 可分性
+ * @returns 可分性を考慮したモザイク量
+ */
 export const convertToMosaicActualAmount = (
   amount: number,
   divisibility: number,
