4700f4dbfadc6659bd594933eb03f0b1bbfe17e2
ry0-cha-ng <ry0changmw@gmail.com>
2024-12-08 17:18:45 +0900

    :sparkles: server/src/info の関数コメントを追加

--- server/src/info/getMultisigInfo.ts
+++ server/src/info/getMultisigInfo.ts
@@ -1,5 +1,10 @@
 import { Config } from "../utils/config"
 
+/**
+ * アカウントのマルチシグ情報を取得する
+ * @param address アドレス
+ * @returns マルチシグ情報
+ */
 export const getMultisigInfo = async (address: string) => {
   return await fetch(new URL(`/account/${address}/multisig`, Config.NODE_URL), {
     method: "GET",
