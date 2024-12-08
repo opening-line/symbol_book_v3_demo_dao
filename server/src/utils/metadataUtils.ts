d6ab3a2fbee4c194d1ca565e9becb26c2f7d028c
ry0-cha-ng <ry0changmw@gmail.com>
2024-12-08 21:40:13 +0900

    :sparkles: metadataUtilsからgenerateMetadataKeyを削除

--- server/src/utils/metadataUtils.ts
+++ server/src/utils/metadataUtils.ts
@@ -1,8 +1,3 @@
-import { metadataGenerateKey } from "symbol-sdk/symbol"
-
-export const generateMetadataKey = (key: string) =>
-  metadataGenerateKey(key).toString(16).toUpperCase()
-
 export const decodeMetadataValue = (value: string) =>
   new TextDecoder().decode(Buffer.from(value, "hex"))
 
